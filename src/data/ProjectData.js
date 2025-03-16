ec4d338fc1c8fc2892cfa65d912207c1a546a783
Karthik Sudarsan <karthik.sudarsan@trenser.com>
2025-03-16 22:19:43 +0530

    update urls of project pdfs

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -4,7 +4,7 @@ const projects = [
     title: "A Crime in Vancouver - A Cluster Analysis",
     description: "An analytical exploration of crime patterns in Vancouver using clustering techniques to identify high-risk neighborhoods.",
     tools: ["Python", "SQL"],
-    pdfLink: "https://drive.google.com/file/d/1A-PqADd1SPsDHKU0DYIb_VVKpvnHzywe/preview", // Updated path
+    pdfLink: "https://drive.google.com/file/d/1RROeFFvnfScolI7DFKFK9gzv7VUx-pOY/preview", // Updated path
     githubLink: "https://github.com/AnupamaSudarsan/Coursera_Capstone.git",
   },
   {
@@ -12,8 +12,7 @@ const projects = [
     title: "PwC Switzerland Power BI Job Simulation on Forage",
     description: "A job simulation creating insightful dashboards, analyzing HR data and communicating data-driven recommendations.",
     tools: ["Power BI"],
-    pdfLink: "https://drive.google.com/file/d/1A-PqADd1SPsDHKU0DYIb_VVKpvnHzywe/view", // Updated path
-    githubLink: "https://github.com/your-repo/pwc-powerbi-simulation",
+    pdfLink: "https://drive.google.com/file/d/1KEwB7wCrLxMIYusvwQTmSUPkMc5uWnQa/preview", // Updated path
   },
 ];
 
